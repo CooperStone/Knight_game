8c1a92c00288e7410d2761dd3cd46b82d5e40d81
Cooper Stone <CStone@stu.kerikerihigh.ac.nz>
2021-06-18 09:29:41 +1200

    first commit

---
+++ testcharacter.py
@@ -0,0 +1,167 @@
+import arcade
+
+SPRITE_SCALING = 0.5
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "testcharacter.py"
+
+MOVEMENT_SPEED = 5
+
+
+class Player(arcade.Sprite):
+    """ Player Class """
+
+    def update(self):
+        """ Move the player """
+        # Move player.
+        # Remove these lines if physics engine is moving player.
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        # Check for out-of-bounds
+        if self.left < 0:
+            self.left = 0
+        elif self.right > SCREEN_WIDTH - 1:
+            self.right = SCREEN_WIDTH - 1
+
+        if self.bottom < 0:
+            self.bottom = 0
+        elif self.top > SCREEN_HEIGHT - 1:
+            self.top = SCREEN_HEIGHT - 1
+
+
+class MyGame(arcade.Window):
+    """
+    Main application class.
+    """
+
+    def __init__(self, width, height, title):
+        """
+        Initializer
+        """
+
+        # Call the parent class initializer
+        super().__init__(width, height, title)
+
+        # Variables that will hold sprite lists
+        self.player_list = None
+
+        # Set up the player info
+        self.player_sprite = None
+
+        # Set the background color
+        arcade.set_background_color(arcade.color.BLACK)
+        self.score = None
+        self.player = None
+        self.coin_list = None
+
+    def setup(self):
+        """ Set up the game and initialize the variables. """
+
+        # Sprite lists
+        self.player_list = arcade.SpriteList()
+
+        # Set up the player
+        self.player_sprite = Player("player_idle.png", SPRITE_SCALING)
+        self.player_sprite.center_x = 50
+        self.player_sprite.center_y = 50
+        self.player_list.append(self.player_sprite)
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        # This command has to happen before we start drawing
+        arcade.start_render()
+
+        # Draw all the sprites.
+        self.player_list.draw()
+
+    def on_update(self, delta_time):
+        """ Movement and game logic """
+
+        # Move the player
+        self.player_list.update()
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+
+        # If the player presses a key, update the speed
+        if key == arcade.key.W:
+            self.player_sprite.change_y = MOVEMENT_SPEED
+        elif key == arcade.key.S:
+            self.player_sprite.change_y = -MOVEMENT_SPEED
+        elif key == arcade.key.A:
+            self.player_sprite.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.D:
+            self.player_sprite.change_x = MOVEMENT_SPEED
+
+        if key == arcade.key.UP:
+            self.player_sprite.change_y = MOVEMENT_SPEED
+        elif key == arcade.key.DOWN:
+            self.player_sprite.change_y = -MOVEMENT_SPEED
+        elif key == arcade.key.LEFT:
+            self.player_sprite.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT:
+            self.player_sprite.change_x = MOVEMENT_SPEED
+
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+
+        # If a player releases a key, zero out the speed.
+        # This doesn't work well if multiple keys are pressed.
+        # Use 'better move by keyboard' example if you need to
+        # handle this.
+        if key == arcade.key.W or key == arcade.key.S:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.A or key == arcade.key.D:
+            self.player_sprite.change_x = 0
+
+        if key == arcade.key.UP or key == arcade.key.DOWN:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.player_sprite.change_x = 0
+
+import random
+def setup(self):
+    self.coin_list = arcade.Spritelist()
+    self.score = 0
+    for i in range(50):
+        coin = arcade.Sprite(':resources:images/items/coinGold.png', 0.5)
+        coin.center_x = random.randint(0, SCREEN_WIDTH)
+        coin.center_x = random.randint(0, SCREEN_HEIGHT)
+        self.coin_list.append(coin)
+
+def on_draw(self):
+    start_render()
+    arcade.draw_text(self.score, WIDTH/2,HEIGHT/2, arcade.color.BLACK, 70)
+    self.player.draw()
+    self.coin_list.draw()
+
+def on_mouse_motion(self, x, y, dx, dy):
+    self.player.center_x = x
+    self.player.center_y = y
+
+def update(self, delta_time):
+    touched_coins = arcade.check_for_collision_with_list(self.player, self.coin_list)
+    for coin in touched_coins:
+        self.score += 1
+        coin.kill()
+    if len(self.coin_list) == 0:
+        self.setup()
+
+def main():
+    """ Main method """
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window.setup()
+    arcade.run()
+
+game = MyGame(600, 600, "mygame")
+game.setup()
+arcade.run()
+
+if __name__ == "__main__":
+    main()
